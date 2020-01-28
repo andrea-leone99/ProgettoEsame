7e53f7a2325b4cab90a65967bf8d8f8c82f52c81
domeletizia <60345802+domeletizia@users.noreply.github.com>
2020-01-28 11:34:37 +0100

    Update Login.java

--- Login.java
+++ Login.java
@@ -6,6 +6,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
+import java.awt.Font;
 
 public class Login {
 
@@ -63,24 +64,32 @@ public class Login {
 		frmLogin.getContentPane().add(lblLogoPng);
 		
 		textField = new JTextField();
-		textField.setBounds(363, 64, 145, 20);
+		textField.setBounds(347, 65, 145, 20);
 		frmLogin.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblUsername = new JLabel("Username");
+		lblUsername.setFont(new Font("Impact", Font.PLAIN, 12));
 		lblUsername.setBounds(261, 67, 76, 14);
 		frmLogin.getContentPane().add(lblUsername);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(261, 136, 64, 14);
+		lblPassword.setFont(new Font("Impact", Font.PLAIN, 12));
+		lblPassword.setBounds(261, 136, 76, 14);
 		frmLogin.getContentPane().add(lblPassword);
 		
 		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(318, 210, 132, 34);
+		btnLogin.setFont(new Font("Impact", Font.PLAIN, 12));
+		btnLogin.setBounds(360, 192, 132, 34);
 		frmLogin.getContentPane().add(btnLogin);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(363, 133, 145, 20);
+		passwordField.setBounds(347, 133, 145, 20);
 		frmLogin.getContentPane().add(passwordField);
+		
+		JLabel lblInserireUsernameE = new JLabel("Inserire username e password per accedere");
+		lblInserireUsernameE.setFont(new Font("Impact", Font.PLAIN, 12));
+		lblInserireUsernameE.setBounds(30, 11, 264, 20);
+		frmLogin.getContentPane().add(lblInserireUsernameE);
 	}
 }
