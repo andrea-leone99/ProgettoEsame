48cd963f8072269d5fac4961fd1860dc04e05a21
Domenico Letizia <60345802+domeletizia@users.noreply.github.com>
2020-02-18 11:52:01 +0100

    ho aggiunto tutti i metodi per il magazzino, la parte estetica,ho inserito il file jar e la relativa icona formato.ico

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    
+    
+  public static void main(String args[]){
+      
+      
+      Login log;
+      log = new Login();
+      log.setVisible(true);
+      
+  }
+   
+}
